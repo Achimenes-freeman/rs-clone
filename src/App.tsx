4623a94ec5f8315aee66ab9d30f8a2900e6a45a2
Achimenes-freeman <gangsta10ip@mail.ru>
2023-02-26 18:36:16 +0300

    feat: create Charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
-// import { useContext } from 'react';
-// import { TestComponent } from './components/TestComponent/TestComponent';
-// import { TestContext } from './context/TestContext/TestContext';
-// import { MainContext } from './context/MainContext/MainContext';
-// import { LineChartGeneric } from './generics/LineChartGeneric/LineChartGeneric';
-import { BarChartGeneric } from "./generics/BarChartGeneric/BarChartGeneric"
+import { useContext } from 'react';
+import { TestComponent } from './components/TestComponent/TestComponent';
+import { TestContext } from './context/TestContext/TestContext';
+import { MainContext } from './context/MainContext/MainContext';
+import { LineChartGeneric } from './generics/LineChartGeneric/LineChartGeneric';
 
 export const App = () => {
 
-    // const {testContext: {printsDynamics}} = useContext(TestContext)
-    // const {isFinished} = useContext(MainContext)
-
-    const infoData = [10,13,16,27,34]
-    const labelsData = ['0-9', '10-19', '20-29', '30-39', '40-49']
+    const {testContext: {printsDynamics}} = useContext(TestContext)
+    const {isFinished} = useContext(MainContext)
 
     return (
         <div className='main'>
-            {/* {isFinished ? <LineChartGeneric printsDynamics={printsDynamics}/>: <TestComponent/>} */}
-
-            <BarChartGeneric infoData={infoData} labelsData={labelsData}/>
+            {isFinished ? <LineChartGeneric printsDynamics={printsDynamics}/>: <TestComponent/>}
         </div>
     )
 }
