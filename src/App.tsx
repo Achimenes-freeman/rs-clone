4e29148f807642c7ace36e3f3d9274743e5269b0
Achimenes-freeman <gangsta10ip@mail.ru>
2023-02-26 10:40:26 +0300

    init: add result page layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useContext } from 'react';
 import { TestComponent } from './components/TestComponent/TestComponent';
 import { MainContext } from './context/MainContext/MainContext';
 
+import { Result } from './components/Result/Result';
+
 export const App = () => {
 
     const {isFinished} = useContext(MainContext)
 
     return (
         <div className='main'>
-            {isFinished ? 'done': <TestComponent/>}
+            {isFinished ? <Result/>: <TestComponent/>}
         </div>
     )
 }
