f74e34ea77e6a91afdf69dac4ee2f2e4d76626fa
Ilia <79699349+iSvitka@users.noreply.github.com>
2023-03-12 16:17:27 +0300

    Feature/adaptive (#19)
    * feat: add adptive for tablets and laptops
    
    * feat: add adaptive for mobile L
    
    * feat: complete adaptive for project"
    
    "

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,6 +4,7 @@ export function Loader() {
     return(
         <div className={styles.Loader}>
             <div className={styles.fakeHeader}>RStype</div>
+            <div className={styles.fakeHeaderShort}>RSt</div>
             <div className={styles.clockLoader}/>
         </div>
     )
--- src/components/ModeBar/ModeBar.tsx
+++ src/components/ModeBar/ModeBar.tsx
@@ -13,14 +13,18 @@ export const ModeBar = () => {
     return (
     <div className={styles.ModeBar}>
         <ul className={styles.modesList}>
-            <li className={cn({[styles.modeActive]: mode === '15 seconds'}, styles.mode)} onClick={()=> changeMode('15 seconds')}>15 seconds</li>
-            <li className={cn({[styles.modeActive]: mode === '30 seconds'}, styles.mode)} onClick={()=> changeMode('30 seconds')}>30 seconds</li>
-            <li className={cn({[styles.modeActive]: mode === '60 seconds'}, styles.mode)} onClick={()=> changeMode('60 seconds')}>60 seconds</li>
-            <li className={cn({[styles.modeActive]: mode === '120 seconds'}, styles.mode)} onClick={()=> changeMode('120 seconds')}>120 seconds</li>
-            <li className={cn({[styles.modeActive]: mode === '10 words'}, styles.mode)} onClick={()=> changeMode('10 words')}>10 words</li>
-            <li className={cn({[styles.modeActive]: mode === '25 words'}, styles.mode)} onClick={()=> changeMode('25 words')}>25 words</li>
-            <li className={cn({[styles.modeActive]: mode === '50 words'}, styles.mode)} onClick={()=> changeMode('50 words')}>50 words</li>
-            <li className={cn({[styles.modeActive]: mode === '100 words'}, styles.mode)} onClick={()=> changeMode('100 words')}>100 words</li>
+            <div className={styles.modesSeconds}>
+                <li className={cn({[styles.modeActive]: mode === '15 seconds'}, styles.mode)} onClick={()=> changeMode('15 seconds')}>15 seconds</li>
+                <li className={cn({[styles.modeActive]: mode === '30 seconds'}, styles.mode)} onClick={()=> changeMode('30 seconds')}>30 seconds</li>
+                <li className={cn({[styles.modeActive]: mode === '60 seconds'}, styles.mode)} onClick={()=> changeMode('60 seconds')}>60 seconds</li>
+                <li className={cn({[styles.modeActive]: mode === '120 seconds'}, styles.mode)} onClick={()=> changeMode('120 seconds')}>120 seconds</li>
+            </div>
+            <div className={styles.modesWords}>
+                <li className={cn({[styles.modeActive]: mode === '10 words'}, styles.mode)} onClick={()=> changeMode('10 words')}>10 words</li>
+                <li className={cn({[styles.modeActive]: mode === '25 words'}, styles.mode)} onClick={()=> changeMode('25 words')}>25 words</li>
+                <li className={cn({[styles.modeActive]: mode === '50 words'}, styles.mode)} onClick={()=> changeMode('50 words')}>50 words</li>
+                <li className={cn({[styles.modeActive]: mode === '100 words'}, styles.mode)} onClick={()=> changeMode('100 words')}>100 words</li>
+            </div>
         </ul>
     </div>
 )}
